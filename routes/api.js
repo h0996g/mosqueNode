48d30687469fdaa2c0d5d34cab724560d9904bbf
h0996g <h0996g@gmail.com>
2024-02-28 10:11:13 +0100

    delete AdminSchema

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userController = require('../controllers/userController')
-const adminController = require('../controllers/adminController')
 const { protect } = require('../handler/auth');
 
 const router = express.Router();
@@ -8,16 +7,9 @@ router.get('/', async (req, res) => {
     res.json({ message: 'hellow' })
 
 })
-
 // ---------------------User---------------------------------------------
 
 router.get('/user', userController.loginUser)
-
 router.post('/user', userController.createUser)
-
-
-// ---------------------admin---------------------------------------------
-router.get('/admin', adminController.loginAdmin)
-router.post('/admin', adminController.createAdmin)
 
 module.exports = router;
